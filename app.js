4bdb5250e478e603c6a57fd7e79ff9a7e07ff870
Viliam <vatvil@gmail.com>
2023-12-02 16:26:17 +0100

    add if statement to watch changes in command.txt

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const fs = require("fs/promises");
 
 (async () => {
-  const watcher = fs.watch("./");
+  const watcher = fs.watch("./command.txt");
 
   for await (const event of watcher) {
-    console.log(event);
+    if (event.eventType === "change") {
+      // the file was changeds
+      console.log("the file was changed");
+    }
   }
-
-  console.log(watcher);
 })();
