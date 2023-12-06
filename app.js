9f83552eb8f04ebcfaedb9c8a21b8b0e91dac283
Viliam <vatvil@gmail.com>
2023-12-06 20:30:38 +0100

    get size of file

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ const fs = require("fs/promises");
     if (event.eventType === "change") {
       // the file was changeds
       console.log("the file was changed");
+      // get the size of the file
+      const size = (await commandFileHandler.stat()).size;
+      const buff = Buffer.alloc(size);
+      const offset = 0;
+      const length = size.byteLength;
+      const position = 0;
+
+      const content = await commandFileHandler.read(
+        buff,
+        offset,
+        length,
+        position
+      );
+      console.log("content", content);
     }
   }
 })();
