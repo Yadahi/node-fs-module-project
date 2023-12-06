7e0fc6f52e99a9caa43686e50ab892dfa86e632e
Viliam <vatvil@gmail.com>
2023-12-06 21:18:40 +0100

    add event emitter and commands to create a file

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
 const fs = require("fs/promises");
 
 (async () => {
+  // Commands
+  const CREATE_FILE = "create a file";
+  const createFile = (path) => {
+    console.log("path", path);
+    fs.writeFile(path, "test");
+  };
+
   const commandFileHandler = await fs.open("./command.txt", "r");
 
+  commandFileHandler.on("change", async () => {
+    // get the size of the file
+    const size = (await commandFileHandler.stat()).size;
+    // allocate our buffer with the size of the file
+    const buff = Buffer.alloc(size);
+    // the location at which we want to start filling our buffer
+    const offset = 0;
+    // how manu bytes we want to read
+    const length = buff.byteLength;
+    // the position that we want to start reading the file from
+    const position = 0;
+
+    // we always want to read the whole content (from beginning all the way to the end)
+    await commandFileHandler.read(buff, offset, length, position);
+    // decoder
+    const command = buff.toString("utf-8");
+
+    // create a file:
+    // create a file <path>
+
+    if (command.includes(CREATE_FILE)) {
+      const filePath = command.substring(CREATE_FILE.length + 1);
+      createFile(filePath);
+    }
+  });
+
+  //   Watcher
   const watcher = fs.watch("./command.txt");
 
   for await (const event of watcher) {
     if (event.eventType === "change") {
-      // the file was changeds
-      console.log("the file was changed");
-      // get the size of the file
-      const size = (await commandFileHandler.stat()).size;
-      const buff = Buffer.alloc(size);
-      const offset = 0;
-      const length = size.byteLength;
-      const position = 0;
-
-      const content = await commandFileHandler.read(
-        buff,
-        offset,
-        length,
-        position
-      );
-      console.log("content", content);
+      // emitting a change handler
+      commandFileHandler.emit("change");
     }
   }
 })();
